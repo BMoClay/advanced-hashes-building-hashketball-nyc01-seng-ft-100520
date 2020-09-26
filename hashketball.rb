cb36686de2d3091fb3b5954fee5ced78ae280253
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-26 17:43:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -55,7 +55,7 @@ def game_hash
             :player_name => "Jason Terry",
           :number => "31",
           :shoe => "15",
-          :points => "",
+          :points => "19",
           :rebounds => "",
           :assists => "",
           :steals => "",
